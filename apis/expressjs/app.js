0a8690e773c1c53f04a4760367de427f8ab041c2
kmtraine106 <kmtrainee.eclerx.com>
2019-12-10 10:17:48 -0500

    document updated

--- apis/expressjs/app.js
+++ apis/expressjs/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
-mongoose.Promise = global.Promise;
+
 
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
